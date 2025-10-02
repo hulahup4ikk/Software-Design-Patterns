4730984e9c453173ab1e4427ac66936cce4dc848
hulahup4ikk <kuzn.vana@gmail.com>
2025-10-02 18:48:37 +0500

    Adapter commit

--- src/Adapter/AdapterComputerWithVGAToMonitorWithHDMI.java
+++ src/Adapter/AdapterComputerWithVGAToMonitorWithHDMI.java
@@ -2,5 +2,9 @@ package Adapter;
 
 public class AdapterComputerWithVGAToMonitorWithHDMI extends ComputerWithVGA implements MonitorWithHDMI {
 
-
+    @Override
+    public void outputVideo() {
+        super.outputVideo();
+        System.out.println("Converting VGA signal to HDMI");
+    }
 }
--- src/Adapter/ComputerWithVGA.java
+++ src/Adapter/ComputerWithVGA.java
@@ -1,7 +1,8 @@
 package Adapter;
 
 public class ComputerWithVGA {
+
     public void outputVideo() {
-        print()
+        System.out.println("Sending video signal through VGA port");
     }
 }
