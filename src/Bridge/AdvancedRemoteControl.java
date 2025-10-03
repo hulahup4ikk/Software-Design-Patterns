fed8028c87449bf75fd3a0ea6e22a1ab0c45a3ba
hulahup4ikk <kuzn.vana@gmail.com>
2025-10-03 15:26:16 +0500

    Bridge commit

--- src/Bridge/AdvancedRemoteControl.java
+++ src/Bridge/AdvancedRemoteControl.java
@@ -1,12 +1,12 @@
 package Bridge;
 
-class AdvancedRemoteControl extends RemoteControl {
+public class AdvancedRemoteControl extends RemoteControl {
     public AdvancedRemoteControl(Device device) {
         super(device);
     }
 
     public void mute() {
+        System.out.println("Advanced remote: mute");
         device.setVolume(0);
-        System.out.println("Muted");
     }
 }
--- src/Bridge/Main.java
+++ src/Bridge/Main.java
@@ -3,17 +3,21 @@ package Bridge;
 public class Main {
     public static void main(String[] args) {
         Device tv = new Tv();
+        System.out.println("Controlling a TV with a basic remote");
         RemoteControl tvRemote = new RemoteControl(tv);
         tvRemote.togglePower();
         tvRemote.volumeUp();
         tvRemote.channelUp();
+        tvRemote.togglePower();
 
         System.out.println("-----");
 
         Device radio = new Radio();
+        System.out.println("\nControlling a Radio with an advanced remote");
         AdvancedRemoteControl radioRemote = new AdvancedRemoteControl(radio);
         radioRemote.togglePower();
         radioRemote.volumeDown();
         radioRemote.mute();
+        radioRemote.togglePower();
     }
 }
