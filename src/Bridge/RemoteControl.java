e7e1b96b26f7dd1c8f38dd9a47e394d2ed0a740b
hulahup4ikk <kuzn.vana@gmail.com>
2025-10-03 15:24:15 +0500

    Simplify bridge example with clearer output

--- src/Bridge/RemoteControl.java
+++ src/Bridge/RemoteControl.java
@@ -1,13 +1,15 @@
 package Bridge;
 
-class RemoteControl {
-    protected Device device;
+public class RemoteControl {
+    protected final Device device;
 
     public RemoteControl(Device device) {
         this.device = device;
+        System.out.println("Remote connected to " + device.getClass().getSimpleName());
     }
 
     public void togglePower() {
+        System.out.println("Remote: toggle power");
         if (device.isEnabled()) {
             device.disable();
         } else {
@@ -16,18 +18,22 @@ class RemoteControl {
     }
 
     public void volumeDown() {
+        System.out.println("Remote: volume down");
         device.setVolume(device.getVolume() - 10);
     }
 
     public void volumeUp() {
+        System.out.println("Remote: volume up");
         device.setVolume(device.getVolume() + 10);
     }
 
     public void channelDown() {
+        System.out.println("Remote: previous channel");
         device.setChannel(device.getChannel() - 1);
     }
 
     public void channelUp() {
+        System.out.println("Remote: next channel");
         device.setChannel(device.getChannel() + 1);
     }
 }
